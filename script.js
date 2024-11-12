7ff5ed85bf5f6fe5bf53c92e39b55222058d654f
18thompsona <milkman9985@gmail.com>
2024-11-11 19:29:23 -0500

    Finished project with rainbow brush selection and autosizing pixels.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector('.container');
-const gridbutton = document.querySelector('#regrid')
+const gridbutton = document.querySelector('#regrid');
+const blackbutton = document.querySelector('#brush-black');
+const rainbowbutton = document.querySelector('#brush-rainbow');
+
+let grid = 16;
+let brush = 'black';
+
 
 gridbutton.addEventListener('click',()=>{
     let temp = prompt("How big the grid? (Max 100)");
@@ -10,10 +16,20 @@ gridbutton.addEventListener('click',()=>{
     }
 })
 
+blackbutton.addEventListener('click',()=>{
+    brush = 'black';
+    console.log(brush);
+})
 
-let grid = 16;
+rainbowbutton.addEventListener('click',()=>{
+    brush = 'rainbow';
+    console.log(brush)
+})
+
 
 function buildGrid(){
+    let pixelSize = 1000 / grid;
+
     for(let i = 0; i < grid; i++){
 
         const subcontainer = document.createElement('div');
@@ -22,9 +38,16 @@ function buildGrid(){
         for(let j = 0; j < grid; j++){
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
+            pixel.style.width = `${pixelSize}px`
+            pixel.style.height = `${pixelSize}px`
 
             pixel.addEventListener("mouseover",()=>{
-                pixel.classList.add('black');
+                if(brush === 'rainbow'){
+                    pixel.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+                }
+                else{
+                    pixel.style.backgroundColor = 'black';
+                }
             })
 
             subcontainer.appendChild(pixel);
